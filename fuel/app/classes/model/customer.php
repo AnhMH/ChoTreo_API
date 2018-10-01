be41804df992895b64d6ab950c5a00a6d537f25a
AnhMH <mhanhqb1@gmail.com>
2018-10-01 23:41:12 +0700

    Modify customer list

--- fuel/app/classes/model/customer.php
+++ fuel/app/classes/model/customer.php
@@ -58,6 +58,15 @@ class Model_Customer extends Model_Abstract {
             ->from(self::$_table_name)
         ;
         
+        // Filter
+        if (!empty($param['keyword'])) {
+            $query->where_open();
+            $query->where(self::$_table_name.'.name', 'LIKE', "%{$param['keyword']}%");
+            $query->or_where(self::$_table_name.'.code', 'LIKE', "%{$param['keyword']}%");
+            $query->or_where(self::$_table_name.'.phone', 'LIKE', "%{$param['keyword']}%");
+            $query->where_close();
+        }
+        
         // Pagination
         if (!empty($param['page']) && $param['limit']) {
             $offset = ($param['page'] - 1) * $param['limit'];
